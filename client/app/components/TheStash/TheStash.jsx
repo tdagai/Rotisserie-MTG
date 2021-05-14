8268a531673d9100e8197a7bcb1af739bdff178e
tdagai <trew3535@gmail.com>
2021-05-13 20:27:21 -0700

    The Stash is now a carousel, will make it more responsive tomorrow

--- client/app/components/TheStash/TheStash.jsx
+++ client/app/components/TheStash/TheStash.jsx
@@ -1,9 +1,43 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import StashItem from './StashItem.jsx';
 import './TheStash.css';
 
 const TheStash = ({ myStash, gridWithStash, setGridWithStash }) => {
   const [collapseStash, setCollapseStash] = useState(false);
+  const [displayedStash, setDisplayedStash] = useState([]);
+  const [carouselIndex, setCarouselIndex] = useState(0);
+
+  useEffect(() => {
+    if (myStash) {
+      // console.log(myStash.length);
+      // console.log(Math.floor((myStash.length - 1) / 10));
+      if (myStash.length <= 10) {
+        setDisplayedStash(myStash);
+      } else {
+        setDisplayedStash(myStash.slice(10 * carouselIndex, (10 * carouselIndex) + 10));
+      }
+    }
+  }, [myStash]);
+
+  useEffect(() => {
+    if (myStash) {
+      setDisplayedStash(myStash.slice(10 * carouselIndex, (10 * carouselIndex) + 10));
+    }
+  }, [carouselIndex])
+
+  const handleNext = (e) => {
+    e.preventDefault();
+    if (carouselIndex < (Math.floor((myStash.length - 1) / 10)) && carouselIndex <= 2) {
+      setCarouselIndex(carouselIndex + 1);
+    }
+  };
+
+  const handleBack = (e) => {
+    e.preventDefault();
+    if (carouselIndex > 0) {
+      setCarouselIndex(carouselIndex - 1);
+    }
+  };
 
   // console.log(document.getElementById('stash-list')?.clientWidth);
 
@@ -22,14 +56,15 @@ const TheStash = ({ myStash, gridWithStash, setGridWithStash }) => {
         !collapseStash &&
         <button
           id='stash-back-button'
-          className='button-style stash-carousel-button' >
+          className='button-style stash-carousel-button'
+          onClick={handleBack} >
           <i className="fas fa-caret-left"></i>
         </button>
       }
       <ul
         id='stash-list'
         className={collapseStash ? 'hide-stash-list' : 'show-stash-list'}>
-        {myStash.map((card) => (
+        {displayedStash.map((card) => (
           <StashItem
             key={card.ff.name}
             collapsed={collapseStash}
@@ -40,7 +75,8 @@ const TheStash = ({ myStash, gridWithStash, setGridWithStash }) => {
         !collapseStash &&
         <button
           id='stash-next-button'
-          className='button-style stash-carousel-button'>
+          className='button-style stash-carousel-button'
+          onClick={handleNext} >
           <i className="fas fa-caret-right"></i>
         </button>
       }
