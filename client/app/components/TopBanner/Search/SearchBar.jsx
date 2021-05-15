96406282d350a35d2a7e3c757675248bc155a2df
tdagai <trew3535@gmail.com>
2021-05-15 15:07:35 -0700

    Extracted Event functions for better code clarity

--- client/app/components/TopBanner/Search/SearchBar.jsx
+++ client/app/components/TopBanner/Search/SearchBar.jsx
@@ -4,25 +4,45 @@ import { SearchContext } from '../../../Contexts/Contexts';
 const SearchBar = ({ searchTerm, handleSearch, setDisplaySearch, setSearchTerm, setErrored }) => {
   const { currentTurn } = useContext(SearchContext);
 
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    if (searchTerm) {
+      handleSearch();
+    } else {
+      setDisplaySearch(false);
+    }
+  };
+
+  const handleFormReset = (e) => {
+    e.preventDefault();
+    setErrored(false);
+    if (searchTerm) {
+      setSearchTerm('');
+      const searchBar = document.querySelector('#search-bar');
+      searchBar.value = '';
+    }
+  };
+
+  const handleInputChange = (e) => {
+    setSearchTerm(e.target.value);
+    setErrored(false);
+  }
+
+  const handleInputBlur = (e) => {
+    e.preventDefault();
+    setDisplaySearch(false);
+  }
+
+  const handleInputFocus = () => {
+    if (searchTerm) {
+      setDisplaySearch(true);
+    }
+  }
+
   return (
     <form
-    onSubmit={(e) => {
-      e.preventDefault();
-      if (searchTerm) {
-        handleSearch();
-      } else {
-        setDisplaySearch(false);
-      }
-    }}
-    onReset={(e) => {
-      e.preventDefault();
-      setErrored(false);
-      if (searchTerm) {
-        setSearchTerm('');
-        const searchBar = document.querySelector('#search-bar');
-        searchBar.value = '';
-      }
-    }}
+    onSubmit={handleFormSubmit}
+    onReset={handleFormReset}
   >
     <input
       id='search-bar'
@@ -33,20 +53,9 @@ const SearchBar = ({ searchTerm, handleSearch, setDisplaySearch, setSearchTerm, 
       autoComplete='false'
       autoCapitalize='none'
       autoCorrect='none'
-      onChange={({ target }) => {
-        setSearchTerm(target.value);
-        setErrored(false);
-      }}
-      onBlur={(e) => {
-      e.preventDefault();
-      setDisplaySearch(false);
-    }}
-      onFocus={() => {
-      if (searchTerm) {
-        setDisplaySearch(true);
-      }
-    }}
-    >
+      onChange={handleInputChange}
+      onBlur={handleInputBlur}
+      onFocus={handleInputFocus} >
     </input>
     { searchTerm && <button aria-label='clear search bar button' id='clear-search-bar-button' type="reset" >X</button> }
   </form>
