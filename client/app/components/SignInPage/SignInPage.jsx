7a3fe0ffa4c8c4a182c8ca0e271fb8737b6dfe90
tdagai <trew3535@gmail.com>
2021-05-19 11:55:59 -0700

    Added responsiveness to sign in and sign up pages

--- client/app/components/SignInPage/SignInPage.jsx
+++ client/app/components/SignInPage/SignInPage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import SignInForm from './SignInForm/SignInForm.jsx';
 import SignUpForm from './SignUpForm/SignUpForm.jsx';
 import './SignInPage.css';
@@ -7,8 +8,8 @@ const SignInPage = ({ inOrUp }) => {
   return (
     <div id='signin-page-container' >
       <div id='signin-page-left' className='signin-page-partition' >
-        <h1 id='signin-app-title-orange' className='signin-app-title' >Rotisserie</h1>
-        <h1 className='signin-app-title' >MTG</h1>
+        <h1><Link to='/' className='signin-app-title' id='signin-app-title-orange' >Rotisserie</Link></h1>
+        <h1><Link to='/' className='signin-app-title' >MTG</Link></h1>
       </div>
       <div id='signin-page-right' className='signin-page-partition' >
         {
