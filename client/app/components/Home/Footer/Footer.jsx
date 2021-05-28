5be9c294926567f1d114e66fab8de8110b9a6bd1
tdagai <trew3535@gmail.com>
2021-05-28 12:51:24 -0700

    All links that lead outside the website now open the url in a new tab. Adjusted footer height and font size

--- client/app/components/Home/Footer/Footer.jsx
+++ client/app/components/Home/Footer/Footer.jsx
@@ -5,12 +5,15 @@ const Footer = () => {
   return (
     <footer id='homepage-footer' >
       <p className='footer-copyright' >Wizards of the Coast, Magic: The Gathering, and their logos are trademarks of Wizards of the Coast LLC in the United States and other countries. © 1993-2021 Wizards. All Rights Reserved.</p>
-      <p className='footer-copyright' >Rotisserie MTG is not affiliated with, endorsed, sponsored, or specifically approved by Wizards of the Coast LLC. Rotisserie MTG may use the trademarks and other intellectual property of Wizards of the Coast LLC, which is permitted under Wizards' Fan Site Policy. MAGIC: THE GATHERING® is a trademark of Wizards of the Coast. For more information about Wizards of the Coast or any of Wizards' trademarks or other intellectual property, please visit their website at https://company.wizards.com/en/legal/fancontentpolicy.</p>
+      <p className='footer-copyright' >Rotisserie MTG is not affiliated with, endorsed, sponsored, or specifically approved by Wizards of the Coast LLC. Rotisserie MTG may use the trademarks and other intellectual property of Wizards of the Coast LLC, which is permitted under Wizards' Fan Site Policy. MAGIC: THE GATHERING® is a trademark of Wizards of the Coast. For more information about Wizards of the Coast or any of Wizards' trademarks or other intellectual property, please visit their website at <a href='https://company.wizards.com/en/legal/fancontentpolicy'
+      target='_blank' className='link-style' >https://company.wizards.com/en/legal/fancontentpolicy</a>.</p>
       <div id='footer-icon-container' >
         <i className="fab fa-discord footer-icon"></i>
         <i className="fab fa-twitter footer-icon"></i>
         <i className="fab fa-instagram footer-icon"></i>
-        <a href='https://github.com/tdagai/Rotisserie-MTG' ><i className="fab fa-github footer-icon"></i></a>
+        <a href='https://github.com/tdagai/Rotisserie-MTG' target='_blank' >
+          <i className="fab fa-github footer-icon"></i>
+        </a>
       </div>
     </footer>
   );
--- client/app/components/UnderConstruction/UnderConstruction.jsx
+++ client/app/components/UnderConstruction/UnderConstruction.jsx
@@ -20,7 +20,7 @@ const UnderConstructions = () => {
         I am a one-man team and so updates might take a little while to come out. I appreciate your understanding.
       </p>
       <p>
-        Follow the project's <a href='https://github.com/tdagai/Rotisserie-MTG' className='link-style' >github</a> for updates on the current state of development!
+        Follow the project's <a href='https://github.com/tdagai/Rotisserie-MTG' target='_blank' className='link-style' >github</a> for updates on the current state of development!
       </p>
       <p><Link to='/' className='link-style'>Go to Home Page</Link></p>
     </div>
