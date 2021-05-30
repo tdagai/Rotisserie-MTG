c084a73d710133b08f3a16cb10f856757b8ef80f
tdagai <trew3535@gmail.com>
2021-05-30 16:10:02 -0700

    Carousel image is now fixed. Carousel now rotates on it's own every 5s. Clicking the arrows resets the timer

--- client/app/components/Home/HeroCarousel/HeroCarousel.jsx
+++ client/app/components/Home/HeroCarousel/HeroCarousel.jsx
@@ -1,10 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PositionIndicator from './PositionIndicator/PositionIndicator.jsx';
 import HeroCarouselItem from './HeroCarouselItem/HeroCarouselItem.jsx'
 import './HeroCarousel.css';
 
 const HeroCarousel = () => {
   const [positionIndex, setPositionIndex] = useState(0);
+  const [timeoutHandle, setTimeoutHandle] = useState('');
+
+  useEffect(() => {
+    let tempHandle = setTimeout(() => {
+      if (positionIndex < 2) {
+        setPositionIndex(positionIndex + 1);
+      } else {
+        setPositionIndex(0);
+      }
+    }, 5000);
+    setTimeoutHandle(tempHandle);
+  }, [positionIndex]);
+
+
   const maxItems = 3;
   const heroItemData = [{
     heroImage: 'https://media.magic.wizards.com/images/wallpaper/Black_Lotus_MTGOweek_Facebook_Wallpaper.jpg',
@@ -12,7 +26,8 @@ const HeroCarousel = () => {
     text: 'Draft Cards From Throughout Magic\'s History'
   },
   {
-    heroImage: 'https://www.artofmtg.com/wp-content/uploads/2016/04/Magnifying-Glass-Shadows-over-Innistrad-Art.jpg',
+    heroImage: 'https://i.imgur.com/Th8a4Xd.jpg',
+    // heroImage: 'https://pbs.twimg.com/media/Ce_T7zUUYAASFcV?format=jpg&name=medium',
     artistCredit: 'Magnifying Glass by Dan Scott',
     text: 'Enjoy A Powerful Search Experience Powered By Scryfall'
   },
@@ -23,17 +38,20 @@ const HeroCarousel = () => {
 
   const styleObj = {
     left: `calc((100vw - 6px) * ${positionIndex} * -1)`,
-    transition: 'left .5s',
-    WebkitTransition: 'left .5s'
+    transition: 'left .75s',
+    WebkitTransition: 'left .75s'
   };
 
   const handleNext = (e) => {
-    e.preventDefault();
+    if (e) {
+      e.preventDefault();
+    }
     if (positionIndex < maxItems - 1) {
       setPositionIndex(positionIndex + 1);
     } else {
       setPositionIndex(0);
     }
+    window.clearTimeout(timeoutHandle);
   }
 
   const handleBack = (e) => {
@@ -43,6 +61,7 @@ const HeroCarousel = () => {
     } else {
       setPositionIndex(maxItems - 1);
     }
+    window.clearTimeout(timeoutHandle);
   }
 
   return (
@@ -56,11 +75,11 @@ const HeroCarousel = () => {
       </button>
       <PositionIndicator positionIndex={positionIndex} />
       <div id='carousel-item-container' style={styleObj} >
-      {
-        heroItemData.map(({ heroImage, artistCredit = '', text }) => (
-          <HeroCarouselItem key={text} heroImage={heroImage} artistCredit={artistCredit} text={text} />
-        ))
-      }
+        {
+          heroItemData.map(({ heroImage, artistCredit = '', text }) => (
+            <HeroCarouselItem key={text} heroImage={heroImage} artistCredit={artistCredit} text={text} />
+          ))
+        }
       </div>
       <button
         id='hero-next-btn'
