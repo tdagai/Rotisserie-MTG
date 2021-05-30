d72960e99118c4ed627fb001968d027888127d80
tdagai <trew3535@gmail.com>
2021-05-29 17:48:11 -0700

    Added most content for the home page, next up is the carousel at the top

--- client/app/components/Home/Footer/Footer.jsx
+++ client/app/components/Home/Footer/Footer.jsx
@@ -8,7 +8,9 @@ const Footer = () => {
       <p className='footer-copyright' >Rotisserie MTG is not affiliated with, endorsed, sponsored, or specifically approved by Wizards of the Coast LLC. Rotisserie MTG may use the trademarks and other intellectual property of Wizards of the Coast LLC, which is permitted under Wizards' Fan Site Policy. MAGIC: THE GATHERING® is a trademark of Wizards of the Coast. For more information about Wizards of the Coast or any of Wizards' trademarks or other intellectual property, please visit their website at <a href='https://company.wizards.com/en/legal/fancontentpolicy'
       target='_blank' className='link-style' >https://company.wizards.com/en/legal/fancontentpolicy</a>.</p>
       <div id='footer-icon-container' >
-        <i className="fab fa-discord footer-icon"></i>
+        <a href='https://discord.gg/XXpR8a43Ts' target='_blank' >
+          <i className="fab fa-discord footer-icon"></i>
+        </a>
         <i className="fab fa-twitter footer-icon"></i>
         <i className="fab fa-instagram footer-icon"></i>
         <a href='https://github.com/tdagai/Rotisserie-MTG' target='_blank' >
