015fa0a28b75a05b97e22cebd769583e16bd7137
tdagai <trew3535@gmail.com>
2021-06-10 16:58:28 -0700

    fixed sign in/up page title and title compatibility

--- client/app/components/SignInPage/SignInPage.jsx
+++ client/app/components/SignInPage/SignInPage.jsx
@@ -12,8 +12,7 @@ const SignInPage = ({ inOrUp }) => {
   return (
     <div id='signin-page-container' >
       <div id='signin-page-left' className='signin-page-partition' >
-        <h1><Link to='/' className='signin-app-title' id='signin-app-title-orange' >Rotisserie</Link></h1>
-        <h1><Link to='/' className='signin-app-title' >MTG</Link></h1>
+        <h1 id='signin-app-title' ><Link to='/' >Rotisserie MTG</Link></h1>
       </div>
       <div id='signin-page-right' className='signin-page-partition' >
         {
